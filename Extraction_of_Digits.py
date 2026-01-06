851834218d61c86a7b2d10a96ce655542ef47e87
Harshit Kadam <harshitkadam822@gmail.com>
2026-01-06 22:21:41 +0530

    lecture 9

--- Extraction_of_Digits.py
+++ Extraction_of_Digits.py
@@ -38,3 +38,86 @@ def count_digits(num):
 print(count_digits(5873))
 
 
+#### check palindrome number
+n = 1234
+num = n
+result = 0
+while num > 0 :
+    last_digit = num % 10
+    result  =result * 10 + last_digit
+    num = num // 10
+if result == n:
+    print("it is a palindrome")
+else:
+    print("not a palindrome")
+
+n = 1221
+num = n
+result = 0
+while num > 0 :
+    last_digit = num % 10
+    result  = result * 10 + last_digit
+    num = num // 10
+if result == n:
+    print("it is a palindrome")
+else:
+    print("not a palindrome")
+
+n = "nitin"
+rev = ""
+
+for ch in n:
+    rev = ch + rev
+
+if rev == n:
+    print("it is a palindrome")
+else:
+    print("not a palindrome")
+
+
+#### Armstrong Number
+n = 153
+num = n
+order = 3
+result = 0
+while num > 0:
+    digit = num % 10
+    ## result += digit ** order
+    result = result + (digit ** order)
+    num = num // 10
+if result == n:
+    print ("it is an armstrong number")
+else:
+    print("not an armstrong number")
+
+
+##### print factor/divisors
+n = 20
+num = n
+result = []
+for i in range (1, num+1):       #(brute force solution)#
+    if num % i == 0:
+        result.append(i)
+print(result)
+
+n = 20
+num = n
+result = []
+for i in range (1, num//2+1):       #(brute force solution)#
+    if num % i == 0:
+        result.append(i)
+#print(result)
+result.append(n)
+print(result)
+
+n = 36
+num = n
+from math import isqrt
+result = []
+for i in range (1, isqrt(num)+1):
+    if num % i == 0:
+        result.append(i)
+        if i !=num // i:
+            result.append(num // i)
+# result.sort()
+print(result)
